e24f0906d67baf6587c9d2a2ae9bbad38e5d0e5c
bossbsj <bossllsj555@gmail.com>
2022-01-28 00:23:00 +0700

    set env

--- src/pinata.tsx
+++ src/pinata.tsx
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-// const key = process.env.REACT_APP_PINATA_KEY
-// const secret = process.env.REACT_APP_PINATA_SECRET
-const key = "e687a54d7a9ef6464618"
-const secret = "b21132320d7fcfc729fa5c4c11e9ccd24ff857b6c0b9d568b957ff50f7d1f1e3"
-
 export const pinJSONToIPFS = async(JSONBody:any) => {
+    const key = process.env.REACT_APP_PINATA_KEY
+    const secret = process.env.REACT_APP_PINATA_SECRET
+    console.log(key, secret)
     const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
     //making axios POST request to Pinata ⬇️
     return axios 
